ec141abd1ac49e8d2d20413b2c199f9f635847c0
l-panhavoan <l-sovanpanhavoan@beniten.com>
2022-03-28 15:12:49 +0700

    first commit

---
+++ backend/apps/order_items/serializers.py
@@ -0,0 +1,17 @@
+from .models import OrderItem
+from rest_framework import serializers
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = [
+            # 'order',
+            'product',
+            'qty'
+        ]
+        read_only_fields = [
+            'order'
+        ]
+        depth = 1
